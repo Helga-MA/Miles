9fd8fe404b2335b75ca5ed7f772f43601f19536f
Olga Stepanova <os20142020@gmail.com>
2021-08-26 03:39:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 2_000;
+        int mile = 20;
+        int freeMiles = price / mile;
+        System.out.println("Free miles: " + freeMiles);
+    }
+}
